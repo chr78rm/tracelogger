bb0370dd2d803c489ca7fb6b3748d4b3632fe492
Christof Reichardt <chr78rm@onlinehome.de>
2018-05-09 14:48:35 +0200

    Javadoc warnings corrected.

--- src/main/java/de/christofreichardt/diagnosis/package-info.java
+++ src/main/java/de/christofreichardt/diagnosis/package-info.java
@@ -1,7 +1,7 @@
 /**
  * <div style="text-align: justify">
- * Provides the basic tracing faclitities. The most important class is {@link AbstractTracer} which is the root
- * of the tracer hierarchy. The {@link TracerFactory} can be feed with an appropriate configuration and thereupon can be used
+ * Provides the basic tracing faclitities. The most important class is {@link de.christofreichardt.diagnosis.AbstractTracer AbstractTracer} which is the root
+ * of the tracer hierarchy. The {@link de.christofreichardt.diagnosis.TracerFactory TracerFactory} can be feed with an appropriate configuration and thereupon can be used
  * to access tracers by name, by thread or from a blocking, bounded queue.
  * </div>
  */
