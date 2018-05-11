ad22452b20f8111ad4d367302c2b26a100a20200
Christof Reichardt <chr78rm@onlinehome.de>
2018-05-11 12:17:12 +0200

    Moved to version 1.8.0

--- src/main/java/de/christofreichardt/diagnosis/AbstractTracer.java
+++ src/main/java/de/christofreichardt/diagnosis/AbstractTracer.java
@@ -99,7 +99,7 @@ import org.w3c.dom.NodeList;
  */
 abstract public class AbstractTracer {
   
-  final static public String VERSION = "1.7.1";
+  final static public String VERSION = "1.8.0";
   
   /**
    * Indicates exceptional states within the AbstractTracer context.
