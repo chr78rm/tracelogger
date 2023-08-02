be6420b337a6429d616f9eaa65595964e7c60abe
Christof Reichardt <chr78rm@onlinehome.de>
2023-08-02 16:12:19 +0200

    Method added for printing a header when starting a test class (unit).

--- src/test/java/de/christofreichardt/diagnosis/BannerPrinter.java
+++ src/test/java/de/christofreichardt/diagnosis/BannerPrinter.java
@@ -6,15 +6,34 @@
 
 package de.christofreichardt.diagnosis;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
 /**
- *
  * @author Developer
  */
 public class BannerPrinter {
-  public void start(String testName, Class<?> clazz) {
-    System.out.println();
-    System.out.printf("-- TEST --%n");
-    System.out.printf("-- TEST --> %s.%s%n", clazz.getSimpleName(), testName);
-    System.out.printf("-- TEST --%n");
-  }
+    public void start(String testName, Class<?> clazz) {
+        System.out.println();
+        System.out.printf("-- TEST --%n");
+        System.out.printf("-- TEST --> %s:%s.%s%n", Thread.currentThread().getName(), clazz.getSimpleName(), testName);
+        System.out.printf("-- TEST --%n");
+    }
+
+    public void startUnit(Class<?> clazz) {
+        String header =
+                "| " +
+                String.format("Starting '%s' at '%s'", clazz.getSimpleName(), LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)) +
+                " |";
+        char[] cells = new char[header.length() - 2];
+        Arrays.fill(cells, '-');
+        String border =
+                '+' +
+                String.valueOf(cells) +
+                '+';
+        System.out.printf("%n%s%n", border);
+        System.out.printf("%s%n", header);
+        System.out.printf("%s%n", border);
+    }
 }
