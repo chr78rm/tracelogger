603444f20945fdb1f9d8ecfa2c87ee3e6782eb3f
Christof Reichardt <chr78rm@onlinehome.de>
2023-08-24 18:01:27 +0200

    Code reformatted, again.

--- src/main/java/de/christofreichardt/diagnosis/net/NetTracer.java
+++ src/main/java/de/christofreichardt/diagnosis/net/NetTracer.java
@@ -22,137 +22,140 @@ import org.w3c.dom.Node;
 
 /**
  * This particular tracer logs over the network.
- * 
+ *
  * @author Christof Reichardt
  */
 public class NetTracer extends AbstractTracer {
-  
-  public static final int TIMEOUT = 5000;
-  
-  /** the default port */
-  final protected int defaultPortNo = 5010;
-  /** the debug server's port number */
-  protected int portNo;
-  /** the host name of the debug server */
-  protected String hostName = "localhost";
-  /** the pizza connection to the debug server */
-  protected Socket pizzaConnection = null;
 
-  public NetTracer(String name) {
-    super(name);
-  }
+    public static final int TIMEOUT = 5000;
 
-  /**
-   * @return the portNo
-   */
-  public int getPortNo() {
-    return portNo;
-  }
+    /**
+     * the default port
+     */
+    final protected int defaultPortNo = 5010;
+    /**
+     * the debug server's port number
+     */
+    protected int portNo;
+    /**
+     * the host name of the debug server
+     */
+    protected String hostName = "localhost";
+    /**
+     * the pizza connection to the debug server
+     */
+    protected Socket pizzaConnection = null;
 
-  /**
-   * @param portNo the portNo to set
-   */
-  public void setPortNo(int portNo) {
-    this.portNo = portNo;
-  }
-
-  /**
-   * @return the hostName
-   */
-  public String getHostName() {
-    return hostName;
-  }
-
-  /**
-   * @param hostName the hostName to set
-   */
-  public void setHostName(String hostName) {
-    this.hostName = hostName;
-  }
-
-  @Override
-  protected void readConfiguration(XPath xpath, Node node) throws XPathExpressionException, Exception {
-    super.readConfiguration(xpath, node);
-    
-    this.hostName = (String) xpath.evaluate("./dns:Listener/dns:Host/text()", node, XPathConstants.STRING);
-    try {
-      this.portNo = Integer.parseInt((String) xpath.evaluate("./dns:Listener/dns:Port/text()", node, XPathConstants.STRING));
+    public NetTracer(String name) {
+        super(name);
     }
-    catch (NumberFormatException ex) {
-      System.err.println(super.getName() + ": Could not parse port number. Using default value[" + this.defaultPortNo + "]");
-      this.portNo = this.defaultPortNo;
+
+    /**
+     * @return the portNo
+     */
+    public int getPortNo() {
+        return portNo;
     }
-    
-    System.out.println("this.hostName = " + this.hostName);
-    System.out.println("this.portNo = " + this.portNo);
-  }
 
-  /**
-   * Opens the associated {@link TracePrintStream} by wrapping the socket streams.
-   */
-  @Override
-  public void open() {
-    try {
-      if (this.isOpened() == false) {
-        System.out.println(formatVersionInfo() + " Opening ...");
-
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(this.hostName, this.portNo);
-        this.pizzaConnection = new Socket();
-        this.pizzaConnection.connect(inetSocketAddress, TIMEOUT);
-        this.setBufferedOutputStream(new BufferedOutputStream(this.pizzaConnection.getOutputStream(), this.getBufferSize()));
-        this.setTracePrintStream(new TracePrintStream(this.getBufferedOutputStream(), this.getThreadMap()));
-
-        sendUserCredentials();
-        this.getTracePrintStream().printf("--> TraceLog opened!%n");
-        this.getTracePrintStream().printf("    Time     : %tc%n", new Date());
-        this.getTracePrintStream().printf("    Bufsize  : %d%n", this.getBufferSize());
-        this.getTracePrintStream().printf("    Autoflush: %b%n%n", this.isAutoflush());
-
-        this.setOpened(true);
-      }
-      else {
-        System.err.println("WARNING: Tracelog is opened already.");
-      }
+    /**
+     * @param portNo the portNo to set
+     */
+    public void setPortNo(int portNo) {
+        this.portNo = portNo;
     }
-    catch (IOException ex) {
-      ex.printStackTrace(System.err);
+
+    /**
+     * @return the hostName
+     */
+    public String getHostName() {
+        return hostName;
     }
-  }
 
-  /**
-   * Closes the associated {@link TracePrintStream}.
-   */
-  @Override
-  public void close() {
-    try {
-      if (this.isOpened() == true) {
-        this.getTracePrintStream().println();
-        this.getTracePrintStream().printf("--> TraceLog closing!%n");
-        this.getTracePrintStream().printf("    Time     : %tc%n", new Date());
-
-        System.out.println(formatStreamErrorState() + " Closing ...");
-
-        this.getTracePrintStream().close();
-        this.getBufferedOutputStream().close();
-        this.pizzaConnection.close();
-
-        this.setOpened(false);
-      }
-      else {
-        System.err.println("WARNING: Tracelog is closed already.");
-      }
+    /**
+     * @param hostName the hostName to set
+     */
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
     }
-    catch (IOException ex) {
-      ex.printStackTrace(System.err);
+
+    @Override
+    protected void readConfiguration(XPath xpath, Node node) throws XPathExpressionException, Exception {
+        super.readConfiguration(xpath, node);
+
+        this.hostName = (String) xpath.evaluate("./dns:Listener/dns:Host/text()", node, XPathConstants.STRING);
+        try {
+            this.portNo = Integer.parseInt((String) xpath.evaluate("./dns:Listener/dns:Port/text()", node, XPathConstants.STRING));
+        } catch (NumberFormatException ex) {
+            System.err.println(super.getName() + ": Could not parse port number. Using default value[" + this.defaultPortNo + "]");
+            this.portNo = this.defaultPortNo;
+        }
+
+        System.out.println("this.hostName = " + this.hostName);
+        System.out.println("this.portNo = " + this.portNo);
     }
-  }
-  
-  /**
-   * Collects some user credentials and sends them over the network.
-   * 
-   * @throws java.net.UnknownHostException if the local host name could not be resolved into an address
-   */
-  protected void sendUserCredentials() throws UnknownHostException {
-    this.getTracePrintStream().printf("user = %s, host = %s, name = %s%n", System.getProperty("user.name"), InetAddress.getLocalHost().getHostName(), super.getName());
-  }
+
+    /**
+     * Opens the associated {@link TracePrintStream} by wrapping the socket streams.
+     */
+    @Override
+    public void open() {
+        try {
+            if (this.isOpened() == false) {
+                System.out.println(formatVersionInfo() + " Opening ...");
+
+                InetSocketAddress inetSocketAddress = new InetSocketAddress(this.hostName, this.portNo);
+                this.pizzaConnection = new Socket();
+                this.pizzaConnection.connect(inetSocketAddress, TIMEOUT);
+                this.setBufferedOutputStream(new BufferedOutputStream(this.pizzaConnection.getOutputStream(), this.getBufferSize()));
+                this.setTracePrintStream(new TracePrintStream(this.getBufferedOutputStream(), this.getThreadMap()));
+
+                sendUserCredentials();
+                this.getTracePrintStream().printf("--> TraceLog opened!%n");
+                this.getTracePrintStream().printf("    Time     : %tc%n", new Date());
+                this.getTracePrintStream().printf("    Bufsize  : %d%n", this.getBufferSize());
+                this.getTracePrintStream().printf("    Autoflush: %b%n%n", this.isAutoflush());
+
+                this.setOpened(true);
+            } else {
+                System.err.println("WARNING: Tracelog is opened already.");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * Closes the associated {@link TracePrintStream}.
+     */
+    @Override
+    public void close() {
+        try {
+            if (this.isOpened() == true) {
+                this.getTracePrintStream().println();
+                this.getTracePrintStream().printf("--> TraceLog closing!%n");
+                this.getTracePrintStream().printf("    Time     : %tc%n", new Date());
+
+                System.out.println(formatStreamErrorState() + " Closing ...");
+
+                this.getTracePrintStream().close();
+                this.getBufferedOutputStream().close();
+                this.pizzaConnection.close();
+
+                this.setOpened(false);
+            } else {
+                System.err.println("WARNING: Tracelog is closed already.");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * Collects some user credentials and sends them over the network.
+     *
+     * @throws java.net.UnknownHostException if the local host name could not be resolved into an address
+     */
+    protected void sendUserCredentials() throws UnknownHostException {
+        this.getTracePrintStream().printf("user = %s, host = %s, name = %s%n", System.getProperty("user.name"), InetAddress.getLocalHost().getHostName(), super.getName());
+    }
 }
