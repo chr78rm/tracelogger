d041778cdbb5d1e98e35c9deba1590051b56fb8e
Christof Reichardt <chr78rm@onlinehome.de>
2023-08-24 16:42:39 +0200

    Code reformatted.

--- src/test/java/de/christofreichardt/diagnosis/TestClass.java
+++ src/test/java/de/christofreichardt/diagnosis/TestClass.java
@@ -17,188 +17,185 @@ import java.util.Random;
  */
 public class TestClass {
 
-  /** the number of loops to perform */
-  static public final int NUMBER_OF_LOOPS = 1000;
+    /**
+     * the number of loops to perform
+     */
+    static public final int NUMBER_OF_LOOPS = 1000;
 
-  private AbstractTracer tracer = null;
-  private boolean emptyStackCorruption = false;
-  private boolean stackOverFlowCorruption = false;
-  private boolean staticCall = false;
+    private AbstractTracer tracer = null;
+    private boolean emptyStackCorruption = false;
+    private boolean stackOverFlowCorruption = false;
+    private boolean staticCall = false;
 
-  /**
-   * Constructor expects a tracer instance.
-   *
-   * @param tracer the tracer to use
-   */
-  public TestClass(AbstractTracer tracer) {
-    this.tracer = tracer;
-  }
-
-  /**
-   * Indicates if an empty stack corruption should be tested. On an average every hundreth call delivers true and as a result 
-   * a call to the entry()-method will be omitted. Subsequently an empty stack corruption occurs during a call of the wayout()- method.
-   *
-   * @return true if a call to the entry()-method should be omitted.
-   */
-  public boolean isEmptyStackCorruption() {
-    boolean flag = false;
-
-    if (this.emptyStackCorruption == true) {
-      Random random = new Random();
-      if (random.nextInt(100) == 25) {
-        flag = true;
-      }
+    /**
+     * Constructor expects a tracer instance.
+     *
+     * @param tracer the tracer to use
+     */
+    public TestClass(AbstractTracer tracer) {
+        this.tracer = tracer;
     }
 
-    return flag;
-  }
+    /**
+     * Indicates if an empty stack corruption should be tested. On an average every hundreth call delivers true and as a result
+     * a call to the entry()-method will be omitted. Subsequently an empty stack corruption occurs during a call of the wayout()- method.
+     *
+     * @return true if a call to the entry()-method should be omitted.
+     */
+    public boolean isEmptyStackCorruption() {
+        boolean flag = false;
 
-  /**
-   * Enables the empty stack corruption test case.
-   *
-   * @param emptyStackCorruption true enables the test case
-   */
-  public void setEmptyStackCorruption(boolean emptyStackCorruption) {
-    this.emptyStackCorruption = emptyStackCorruption;
-  }
+        if (this.emptyStackCorruption == true) {
+            Random random = new Random();
+            if (random.nextInt(100) == 25) {
+                flag = true;
+            }
+        }
 
-  /**
-   * Indicates if a stack overflow should be provoked. On an average every tenth call delivers true and as a result 
-   * a call to a wayout()-method will be omitted. Subsequently the stack size overflows during a call of the entry()-method.
-   *
-   * @return true if a call to the wayout()-method should be omitted.
-   */
-  public boolean isStackOverFlowCorruption() {
-    boolean flag = false;
-
-    if (this.stackOverFlowCorruption == true) {
-      Random random = new Random();
-      if (random.nextInt(100) >= 90) {
-        flag = true;
-      }
+        return flag;
     }
 
-    return flag;
-  }
+    /**
+     * Enables the empty stack corruption test case.
+     *
+     * @param emptyStackCorruption true enables the test case
+     */
+    public void setEmptyStackCorruption(boolean emptyStackCorruption) {
+        this.emptyStackCorruption = emptyStackCorruption;
+    }
 
-  /**
-   * Enables the stack overflow test case.
-   *
-   * @param stackOverFlowCorruption true enables the test case
-   */
-  public void setStackOverFlowCorruption(boolean stackOverFlowCorruption) {
-    this.stackOverFlowCorruption = stackOverFlowCorruption;
-  }
+    /**
+     * Indicates if a stack overflow should be provoked. On an average every tenth call delivers true and as a result
+     * a call to a wayout()-method will be omitted. Subsequently the stack size overflows during a call of the entry()-method.
+     *
+     * @return true if a call to the wayout()-method should be omitted.
+     */
+    public boolean isStackOverFlowCorruption() {
+        boolean flag = false;
 
-  /**
-   * Indicates that a static call should be made.
-   *
-   * @param staticCall the staticCall to set
-   */
-  public void setStaticCall(boolean staticCall) {
-    this.staticCall = staticCall;
-  }
+        if (this.stackOverFlowCorruption == true) {
+            Random random = new Random();
+            if (random.nextInt(100) >= 90) {
+                flag = true;
+            }
+        }
 
-  /*
-   * element functions
-   */
-  /**
-   * First level test method.
-   */
-  public void performTests() {
-    this.tracer.entry("void", this, "performTests()");
+        return flag;
+    }
 
-    try {
-      synchronized (this.tracer.getSyncObject()) {
-        this.tracer.out().printfIndentln("%s is performing tests ...", Thread.currentThread().getName());
-        this.tracer.out().printfIndentln("%s: this.numberOfLoops = %d", Thread.currentThread().getName(), TestClass.NUMBER_OF_LOOPS);
-      }
+    /**
+     * Enables the stack overflow test case.
+     *
+     * @param stackOverFlowCorruption true enables the test case
+     */
+    public void setStackOverFlowCorruption(boolean stackOverFlowCorruption) {
+        this.stackOverFlowCorruption = stackOverFlowCorruption;
+    }
 
-      for (int i = 0; i < TestClass.NUMBER_OF_LOOPS; i++) {
+    /**
+     * Indicates that a static call should be made.
+     *
+     * @param staticCall the staticCall to set
+     */
+    public void setStaticCall(boolean staticCall) {
+        this.staticCall = staticCall;
+    }
+
+    /*
+     * element functions
+     */
+
+    /**
+     * First level test method.
+     */
+    public void performTests() {
+        this.tracer.entry("void", this, "performTests()");
+
         try {
-          synchronized (this.tracer.getSyncObject()) {
-            this.tracer.out().printfIndentln("%s: i = %d", Thread.currentThread().getName(), i);
-          }
+            synchronized (this.tracer.getSyncObject()) {
+                this.tracer.out().printfIndentln("%s is performing tests ...", Thread.currentThread().getName());
+                this.tracer.out().printfIndentln("%s: this.numberOfLoops = %d", Thread.currentThread().getName(), TestClass.NUMBER_OF_LOOPS);
+            }
 
-          firstTestMethod(i);
+            for (int i = 0; i < TestClass.NUMBER_OF_LOOPS; i++) {
+                try {
+                    synchronized (this.tracer.getSyncObject()) {
+                        this.tracer.out().printfIndentln("%s: i = %d", Thread.currentThread().getName(), i);
+                    }
+
+                    firstTestMethod(i);
+                } catch (java.lang.Exception ex) {
+                    this.tracer.logException(LogLevel.WARNING, ex, this.getClass(), "performTests()");
+                }
+            }
+
+            this.tracer.logMessage(LogLevel.WARNING, "Nur zum testen.", this.getClass(), "performTests()");
+        } finally {
+            this.tracer.wayout();
         }
-        catch (java.lang.Exception ex) {
-          this.tracer.logException(LogLevel.WARNING, ex, this.getClass(), "performTests()");
+    }
+
+    private void firstTestMethod(int i) throws java.lang.Exception {
+        this.tracer.entry("void", this, "firstTestMethod(int i)");
+
+        try {
+            synchronized (this.tracer.getSyncObject()) {
+                this.tracer.out().printfIndentln("%s is within first test method.", Thread.currentThread().getName());
+            }
+
+            secondTestMethod(i);
+            thirdTestMethod(i);
+        } finally {
+            this.tracer.wayout();
         }
-      }
-
-      this.tracer.logMessage(LogLevel.WARNING, "Nur zum testen.", this.getClass(), "performTests()");
-    }
-    finally {
-      this.tracer.wayout();
-    }
-  }
-
-  private void firstTestMethod(int i) throws java.lang.Exception {
-    this.tracer.entry("void", this, "firstTestMethod(int i)");
-
-    try {
-      synchronized (this.tracer.getSyncObject()) {
-        this.tracer.out().printfIndentln("%s is within first test method.", Thread.currentThread().getName());
-      }
-
-      secondTestMethod(i);
-      thirdTestMethod(i);
-    }
-    finally {
-      this.tracer.wayout();
-    }
-  }
-
-  private void secondTestMethod(int i) throws java.lang.Exception {
-    if (!isEmptyStackCorruption()) {
-      this.tracer.entry("void", this, "secondTestMethod(int i)");
     }
 
-    try {
-      synchronized (this.tracer.getSyncObject()) {
-        this.tracer.out().printfIndentln("%s is within second test method.", Thread.currentThread().getName());
-      }
+    private void secondTestMethod(int i) throws java.lang.Exception {
+        if (!isEmptyStackCorruption()) {
+            this.tracer.entry("void", this, "secondTestMethod(int i)");
+        }
 
-      thirdTestMethod(i);
-      if (this.staticCall) {
-        staticCall(i, this.tracer);
-      }
-    }
-    finally {
-      if (!isStackOverFlowCorruption()) {
-        this.tracer.wayout();
-      }
-    }
-  }
+        try {
+            synchronized (this.tracer.getSyncObject()) {
+                this.tracer.out().printfIndentln("%s is within second test method.", Thread.currentThread().getName());
+            }
 
-  private void thirdTestMethod(int i) throws java.lang.Exception {
-    this.tracer.entry("void", this, "thirdTestMethod(int i)");
-
-    try {
-      synchronized (this.tracer.getSyncObject()) {
-        this.tracer.out().printfIndentln("%s is within third test method.", Thread.currentThread().getName());
-      }
-
-      if (i % 10 == 0) {
-        throw new java.lang.Exception("Eine Exception zu Testzwecken.");
-      }
+            thirdTestMethod(i);
+            if (this.staticCall) {
+                staticCall(i, this.tracer);
+            }
+        } finally {
+            if (!isStackOverFlowCorruption()) {
+                this.tracer.wayout();
+            }
+        }
     }
-    finally {
-      this.tracer.wayout();
-    }
-  }
 
-  private static void staticCall(int i, AbstractTracer tracer) throws java.lang.Exception {
-    tracer.entry("void", TestClass.class, "staticCall(int i, AbstractTracer tracer)");
+    private void thirdTestMethod(int i) throws java.lang.Exception {
+        this.tracer.entry("void", this, "thirdTestMethod(int i)");
 
-    try {
-      synchronized (tracer.getSyncObject()) {
-        tracer.out().printfIndentln("%s is within the static call.", Thread.currentThread().getName());
-      }
+        try {
+            synchronized (this.tracer.getSyncObject()) {
+                this.tracer.out().printfIndentln("%s is within third test method.", Thread.currentThread().getName());
+            }
+
+            if (i % 10 == 0) {
+                throw new java.lang.Exception("Eine Exception zu Testzwecken.");
+            }
+        } finally {
+            this.tracer.wayout();
+        }
     }
-    finally {
-      tracer.wayout();
+
+    private static void staticCall(int i, AbstractTracer tracer) throws java.lang.Exception {
+        tracer.entry("void", TestClass.class, "staticCall(int i, AbstractTracer tracer)");
+
+        try {
+            synchronized (tracer.getSyncObject()) {
+                tracer.out().printfIndentln("%s is within the static call.", Thread.currentThread().getName());
+            }
+        } finally {
+            tracer.wayout();
+        }
     }
-  }
 }
--- src/test/java/de/christofreichardt/diagnosis/TestRunnable.java
+++ src/test/java/de/christofreichardt/diagnosis/TestRunnable.java
@@ -13,62 +13,60 @@ import java.util.concurrent.CyclicBarrier;
  */
 public class TestRunnable implements Runnable {
 
-  private AbstractTracer tracer = null;
-  private CyclicBarrier cyclicBarrier = null;
-  private TestClass testObject = null;
+    private AbstractTracer tracer = null;
+    private CyclicBarrier cyclicBarrier = null;
+    private TestClass testObject = null;
 
-  /**
-   * Creates a new instance of TestRunnable
-   *
-   * @param tracer the tracer to test
-   */
-  public TestRunnable(AbstractTracer tracer) {
-    this.tracer = tracer;
-  }
-
-  /**
-   * Calls a test class which will perform some tests.
-   */
-  @Override
-  public void run() {
-    this.tracer.initCurrentTracingContext(5, true);
-    this.tracer.entry("void", this, "run()");
-
-    try {
-      try {
-        this.cyclicBarrier.await();
-      }
-      catch (InterruptedException | BrokenBarrierException ex) {
-        ex.printStackTrace(System.err);
-      }
-
-      if (this.testObject == null) {
-        this.testObject = new TestClass(this.tracer);
-      }
-
-      this.testObject.performTests();
+    /**
+     * Creates a new instance of TestRunnable
+     *
+     * @param tracer the tracer to test
+     */
+    public TestRunnable(AbstractTracer tracer) {
+        this.tracer = tracer;
     }
-    finally {
-      this.tracer.wayout();
+
+    /**
+     * Calls a test class which will perform some tests.
+     */
+    @Override
+    public void run() {
+        this.tracer.initCurrentTracingContext(5, true);
+        this.tracer.entry("void", this, "run()");
+
+        try {
+            try {
+                this.cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException ex) {
+                ex.printStackTrace(System.err);
+            }
+
+            if (this.testObject == null) {
+                this.testObject = new TestClass(this.tracer);
+            }
+
+            this.testObject.performTests();
+        } finally {
+            this.tracer.wayout();
+        }
     }
-  }
 
-  /**
-   * cyclicBarrier setter.
-   *
-   * @param cyclicBarrier a synchronization aid
-   */
-  public void setCyclicBarrier(CyclicBarrier cyclicBarrier) {
-    this.cyclicBarrier = cyclicBarrier;
-  }
+    /**
+     * cyclicBarrier setter.
+     *
+     * @param cyclicBarrier a synchronization aid
+     */
+    public void setCyclicBarrier(CyclicBarrier cyclicBarrier) {
+        this.cyclicBarrier = cyclicBarrier;
+    }
 
-  /**
-   * testClass setter.
-   *
-   * @param testClass a test instance
-   */
-  public void setTestObject(TestClass testObject) {
-    this.testObject = testObject;
-  }
+    /**
+     * testClass setter.
+     *
+     * @param testObject a test instance
+     */
+    public void setTestObject(TestClass testObject) {
+        this.testObject = testObject;
+    }
 
 }
