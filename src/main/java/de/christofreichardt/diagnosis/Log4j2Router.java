3eddef7fc0e29b4d78eb183246c643fc4083981a
Christof Reichardt <chr78rm@onlinehome.de>
2023-12-05 13:05:25 +0100

    Javadoc comments added to class Log4j2Router.

--- src/main/java/de/christofreichardt/diagnosis/Log4j2Router.java
+++ src/main/java/de/christofreichardt/diagnosis/Log4j2Router.java
@@ -11,7 +11,10 @@ import org.apache.logging.log4j.message.SimpleMessage;
 import org.apache.logging.log4j.spi.ExtendedLogger;
 
 /**
- * @author developer
+ * A specialized {@link NullTracer} which delegates log messages to Log4j2 loggers. For more information about Log4j2, see
+ * <a href="https://logging.apache.org/log4j/2.x/">Apache Log4j</a>.
+ *
+ * @author Christof Reichardt
  */
 public class Log4j2Router extends NullTracer {
 
@@ -34,12 +37,30 @@ public class Log4j2Router extends NullTracer {
         return level;
     }
 
+    /**
+     * This method gets itself a Log4j2 <a href="https://logging.apache.org/log4j/2.x/javadoc/log4j-api/org/apache/logging/log4j/spi/ExtendedLogger.html">ExtendedLogger</a> by invoking Log4j2s
+     * LogManager with the given clazz. Thereupon the {@code ExtendedLogger} will be used to log the given message.
+     *
+     * @param logLevel will be converted to Log4j2s level
+     * @param message  the to be logged message
+     * @param clazz    the originating class
+     * @param methodName the originating method
+     */
     @Override
     public void logMessage(LogLevel logLevel, String message, Class<?> clazz, String methodName) {
         ExtendedLogger extendedLogger = (ExtendedLogger) LogManager.getLogger(clazz);
         extendedLogger.logMessage(Log4j2Router.class.getName(), convertToLogbackLevel(logLevel), null, new SimpleMessage(message), null);
     }
 
+    /**
+     * This method gets itself a Log4j2 <a href="https://logging.apache.org/log4j/2.x/javadoc/log4j-api/org/apache/logging/log4j/spi/ExtendedLogger.html">ExtendedLogger</a> by invoking Log4j2s
+     * LogManager with the given clazz. Thereupon the {@code ExtendedLogger} will be used to log the given throwable.
+     *
+     * @param logLevel  will be converted to Log4j2s level
+     * @param throwable the to be logged throwable
+     * @param clazz     the originating class
+     * @param methodName the name of the relevant method
+     */
     @Override
     public void logException(LogLevel logLevel, Throwable throwable, Class<?> clazz, String methodName) {
         ExtendedLogger extendedLogger = (ExtendedLogger) LogManager.getLogger(clazz);
