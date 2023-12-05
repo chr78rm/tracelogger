d74869a51d84bd17c647bd8e317cfee18d981621
Christof Reichardt <chr78rm@onlinehome.de>
2023-12-05 14:02:04 +0100

    Javadoc comments added to class LogbackRouter.

--- src/main/java/de/christofreichardt/diagnosis/LogbackRouter.java
+++ src/main/java/de/christofreichardt/diagnosis/LogbackRouter.java
@@ -14,6 +14,9 @@ import org.slf4j.spi.LocationAwareLogger;
 import org.w3c.dom.Node;
 
 /**
+ * A specialized {@link NullTracer} which delegates log messages to Logback loggers. For more information about Logback, see
+ * <a href="https://logback.qos.ch/">Logback</a>.
+ *
  * @author Christof Reichardt
  */
 final public class LogbackRouter extends NullTracer {
@@ -38,12 +41,32 @@ final public class LogbackRouter extends NullTracer {
         return Level.toLocationAwareLoggerInteger(level);
     }
 
+    /**
+     * This method gets itself a <a href="https://www.slf4j.org/apidocs/org/slf4j/spi/LocationAwareLogger.html">LocationAwareLogger</a> by invoking SLF4Js
+     * <a href="https://www.slf4j.org/apidocs/org/slf4j/LoggerFactory.html">LoggerFactory</a> with the given clazz. Thereupon the {@code LocationAwareLogger} will be used
+     * to log the given {@code message}.
+     *
+     * @param logLevel will be converted to an appropriate Logback level
+     * @param message  the to be logged message
+     * @param clazz    the originating class
+     * @param methodName the originating method
+     */
     @Override
     public void logMessage(LogLevel logLevel, String message, Class<?> clazz, String methodName) {
         LocationAwareLogger logger = (LocationAwareLogger) LoggerFactory.getLogger(clazz);
         logger.log(null, LogbackRouter.class.getName(), convertToLogbackLevel(logLevel), message, null, null);
     }
 
+    /**
+     * This method gets itself a <a href="https://www.slf4j.org/apidocs/org/slf4j/spi/LocationAwareLogger.html">LocationAwareLogger</a> by invoking SLF4Js
+     * <a href="https://www.slf4j.org/apidocs/org/slf4j/LoggerFactory.html">LoggerFactory</a> with the given clazz. Thereupon the {@code LocationAwareLogger} will be used
+     * to log the given {@code throwable}.
+     *
+     * @param logLevel  will be converted to an appropriate Logback level
+     * @param throwable the to be logged throwable
+     * @param clazz     the originating class
+     * @param methodName the name of the relevant method
+     */
     @Override
     public void logException(LogLevel logLevel, Throwable throwable, Class<?> clazz, String methodName) {
         LocationAwareLogger logger = (LocationAwareLogger) LoggerFactory.getLogger(clazz);
